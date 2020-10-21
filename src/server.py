0bd23abb17247ee2ffdc894bcfaee2650ca93344
Raul Alcantara <ralcanta@mit.edu>
2020-10-21 02:00:56 -0400

    updated python server to allow multiple WebRTC peer connections

--- src/server.py
+++ src/server.py
@@ -15,11 +15,16 @@ from av import VideoFrame
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
 
+from collections import defaultdict 
+
 ROOT = os.path.dirname(__file__)
 
 logger = logging.getLogger("pc")
 existingSockets = []
 pcs = []
+
+video_objects = dict() #id -> VideoTransformTrack
+
 # serverHostName = 'https://viral-aroma.herokuapp.com/'
 sio = socketio.AsyncServer()
 
@@ -30,66 +35,76 @@ class VideoTransformTrack(MediaStreamTrack):
 
     kind = "video"
 
-    def __init__(self, track, transform):
+    def __init__(self, track, transform, own_id, peer_id):
         super().__init__()  # don't forget this!
         self.track = track
         self.transform = transform
+        self.own_id = own_id 
+        self.peer_id = peer_id
 
     async def recv(self):
-        frame = await self.track.recv()
+        if self.peer_id in video_objects:
+            print(f"[{self.own_id}]: Showing other video..")
+            frame = await video_objects[self.peer_id].track.recv()
+            peer_transform = video_objects[self.peer_id].transform
+            # frame = await self.track.recv()
 
-        if self.transform == "cartoon":
-            img = frame.to_ndarray(format="bgr24")
+            if peer_transform == "cartoon":
+                img = frame.to_ndarray(format="bgr24")
 
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+                # prepare color
+                img_color = cv2.pyrDown(cv2.pyrDown(img))
+                for _ in range(6):
+                    img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+                img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+                # prepare edges
+                img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                img_edges = cv2.adaptiveThreshold(
+                    cv2.medianBlur(img_edges, 7),
+                    255,
+                    cv2.ADAPTIVE_THRESH_MEAN_C,
+                    cv2.THRESH_BINARY,
+                    9,
+                    2,
+                )
+                img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
 
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
-            # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "edges":
-            # perform edge detection
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+                # combine color and edges
+                img = cv2.bitwise_and(img_color, img_edges)
+                # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                # rebuild a VideoFrame, preserving timing information
+                new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+                new_frame.pts = frame.pts
+                new_frame.time_base = frame.time_base
+                return new_frame
+            elif peer_transform == "edges":
+                # perform edge detection
+                img = frame.to_ndarray(format="bgr24")
+                img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "rotate":
-            # rotate image
-            img = frame.to_ndarray(format="bgr24")
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
+                # rebuild a VideoFrame, preserving timing information
+                new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+                new_frame.pts = frame.pts
+                new_frame.time_base = frame.time_base
+                return new_frame
+            elif peer_transform == "rotate":
+                # rotate image
+                img = frame.to_ndarray(format="bgr24")
+                rows, cols, _ = img.shape
+                M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+                img = cv2.warpAffine(img, M, (cols, rows))
 
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
+                # rebuild a VideoFrame, preserving timing information
+                new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+                new_frame.pts = frame.pts
+                new_frame.time_base = frame.time_base
+                return new_frame
+            else:
+                return frame
         else:
+            print(f"[{self.own_id}]: Showing own video...")
+            frame = await self.track.recv()
             return frame
 
 async def index(request):
@@ -120,12 +135,18 @@ async def offer(request):
     @pc.on("track") # ???
     def on_track(track):
         log_info("Track %s received", track.kind)
-
+        print("received track!")
         if track.kind == "video":
             log_info(params["video_transform"])
             local_video = VideoTransformTrack(
-                track, transform=params["video_transform"]
+                track, 
+                transform=params["video_transform"], 
+                own_id=params["from"], 
+                peer_id=params["to"]
             )
+            print(f"from={params['from']} and to={params['to']}")
+            print(len(video_objects))
+            video_objects[params["from"]] = local_video
             pc.addTrack(local_video)  
         @track.on("ended")
         async def on_ended():
@@ -135,6 +156,11 @@ async def offer(request):
     # send answer
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)    
+    
+    if params["is_caller"]:
+        await sio.emit("create-connection", {
+            "from": params["from"]
+        }, to=params["to"])
 
     # await sio.emit("call-made", {
     #     "offer": params["offer"],
