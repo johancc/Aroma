b6cd033b3bbba089815d3893b1c96de6c4ffb804
Raul Alcantara <ralcanta@mit.edu>
2020-10-21 02:03:26 -0400

    updated index.js to use python server

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,7 +6,10 @@ const existingCalls = [];
 
 const { RTCPeerConnection, RTCSessionDescription } = window;
 
-const peerConnection = new RTCPeerConnection();
+var config = {
+  sdpSemantics: 'unified-plan'
+};
+const peerConnection = new RTCPeerConnection(config);
 
 function unselectUsersFromList() {
   const alreadySelectedUser = document.querySelectorAll(
@@ -35,40 +38,66 @@ function createUserItemContainer(socketId) {
     userContainerEl.setAttribute("class", "active-user active-user--selected");
     const talkingWithInfo = document.getElementById("talking-with-info");
     talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
-    callUser(socketId);
+    callUser(socketId, true, "rotate");
   });
 
   return userContainerEl;
 }
 
-async function callUser(socketId) {
-  const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+async function callUser(socketId, is_caller, mode) {
+  // const offer = await peerConnection.createOffer();
+  // console.log("setting local description")
+  // await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+  // await peerConnection.setLocalDescription(offer);
+  
 
-  socket.emit("call-user", {
-    offer,
-    to: socketId
-  });
+  // socket.emit("call-user", {
+  //   offer,
+  //   to: socketId
+  // });
   // this fetch below is an attempt to do something similar to https://github.com/aiortc/aiortc/blob/ea116d073129a022805b0a0741ae82a68c4cf3ca/examples/server/client.js#L84
-  var offerReceived =  peerConnection.localDescription;
-  var codec;
-  return fetch('/call-user', { 
-    body: JSON.stringify({
-      sdp: offerReceived.sdp,
-      type: offerReceived.type, 
-      video_transform: "edges", //TODO: need to make it interactive, not hardcode
-      offer: offer,
-      to: socketId,
-      from: socket.id
-    }),
-    headers:{
-      'Content-Type': "application/json",
-    },
-    method: 'POST'
-  }).then(function(response){
+
+  return peerConnection.createOffer().then(function(offer) {
+      return peerConnection.setLocalDescription(offer);
+  }).then(function() {
+          // wait for ICE gathering to complete
+          console.log("uau")
+          return new Promise(function(resolve) {
+              if (peerConnection.iceGatheringState === 'complete') {
+                  resolve();
+              } else {
+                  function checkState() {
+                      if (peerConnection.iceGatheringState === 'complete') {
+                          peerConnection.removeEventListener('icegatheringstatechange', checkState);
+                          resolve();
+                      }
+                  }
+                  peerConnection.addEventListener('icegatheringstatechange', checkState);
+              }
+          });
+  }).then(function(){
+    var offerReceived = peerConnection.localDescription;
+    return fetch('/call-user', { 
+      body: JSON.stringify({
+        sdp: offerReceived.sdp,
+        type: offerReceived.type, 
+        video_transform: mode, //TODO: need to make it interactive, not hardcode
+        is_caller: is_caller,
+        offer: offerReceived,
+        to: socketId,
+        from: socket.id
+      }),
+      headers:{
+        'Content-Type': "application/json",
+      },
+      method: 'POST'
+    })
+  })
+  .then(function(response){
     return response.json()
   }).then(function(answer){
-    console.log("Got answer: sdp: "+answer.sdp + " type: "+ answer.type)
+    // console.log("Got answer: sdp: "+answer.sdp + " type: "+ answer.type)
+    console.log("setting remote description")
     return peerConnection.setRemoteDescription(answer);
   }).catch(function(e){
     alert(e);
@@ -95,6 +124,9 @@ socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
 });
 
+socket.on("create-connection", async data =>{
+  await callUser(data.from, false, "edges");
+})
 socket.on("remove-user", ({ socketId }) => {
   const elToRemove = document.getElementById(socketId);
 
@@ -104,24 +136,25 @@ socket.on("remove-user", ({ socketId }) => {
 });
 
 socket.on("call-made", async data => {
-  if (getCalled) {
-    const confirmed = confirm(
-      `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
-    );
+  // if (getCalled) {
+  //   const confirmed = confirm(
+  //     `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
+  //   );
 
-    if (!confirmed) {
-      socket.emit("reject-call", {
-        from: data.socket
-      });
+  //   if (!confirmed) {
+  //     socket.emit("reject-call", {
+  //       from: data.socket
+  //     });
 
-      return;
-    }
-  }
-
+  //     return;
+  //   }
+  // }
+  console.log("receiver setting remote description")
   await peerConnection.setRemoteDescription(
     new RTCSessionDescription(data.offer)
   );
   const answer = await peerConnection.createAnswer();
+  console.log("receiver setting local description")
   await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
 
   socket.emit("make-answer", {
@@ -132,6 +165,7 @@ socket.on("call-made", async data => {
 });
 
 socket.on("answer-made", async data => {
+  console.log("caller setting remote description")
   await peerConnection.setRemoteDescription(
     new RTCSessionDescription(data.answer)
   );
@@ -147,12 +181,21 @@ socket.on("call-rejected", data => {
   unselectUsersFromList();
 });
 
-peerConnection.ontrack = function({ streams: [stream] }) {
-  const remoteVideo = document.getElementById("remote-video");
-  if (remoteVideo) {
-    remoteVideo.srcObject = stream;
+// connect audio / video
+peerConnection.addEventListener('track', function(evt) {
+  console.log("getting track...")
+  if (evt.track.kind == 'video'){
+      document.getElementById('remote-video').srcObject = evt.streams[0];
   }
-};
+  else
+      document.getElementById('audio').srcObject = evt.streams[0];
+});
+// peerConnection.ontrack = function({ streams: [stream] }) {
+//   const remoteVideo = document.getElementById("remote-video");
+//   if (remoteVideo) {
+//     remoteVideo.srcObject = stream;
+//   }
+// };
 
 navigator.getUserMedia(
   { video: true, audio: true },
